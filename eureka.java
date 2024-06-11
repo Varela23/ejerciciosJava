6945c7c434e942eef2e2d6a784c38f17081c42b7
oscar <ingenierovarela@hotmail.com>
2024-06-10 21:24:42 -0500

    eureka

---
+++ eureka.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class eureka {
+
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Digite su palabra:");
+        String frase=leer.nextLine();
+    }
+}
